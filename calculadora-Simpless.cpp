8afd05b7debb6a5814c41dd501576b530919ca93
Zeus Xaloc Developer <85583693+ZeusXaloc-Dev@users.noreply.github.com>
2021-06-21 15:09:24 -0400

    Add files via upload

---
+++ calculadora-Simpless.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  long double num1, num2, res;
+  char cond;
+
+
+ cout<<"digite o primeiro número";
+ cin>>num1;
+ cout<<"digite a operação desejada";
+ cin>>cond;
+ cout<<"digite o segundo número";
+ cin>>num2;
+ if(cond == '+'){
+     res=num1+num2;   
+    }
+     if(cond == '-'){
+     res=num1-num2;   
+    }
+     if(cond == '/'){
+     res=num1/num2;   
+    }
+     if(cond == '*'){
+     res=num1*num2;   
+    }
+    cout<<"o resultado é:"<<res;
+    
+    
+}
+
